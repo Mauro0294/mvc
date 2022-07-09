37b6bf8756af47d09ca1970fdb70050b76adef05
Mauro Scheltens <mauroscheltens@hotmail.com>
2022-07-09 22:02:02 +0200

    feat: index with routing to controllers

---
+++ controllers/UserController.class.php
@@ -0,0 +1,14 @@
+<?php
+class UserController
+{
+    
+    public function login()
+    {
+        echo '<h1>User login</h1>';
+    }
+    public function register()
+    {
+        echo "<h1>User register</h1>";
+    }
+}
+?>
